758d7b8f1c90797928453301974c58b32f90115f
Patrick Aucoin <aucoinp640@gmail.com>
2023-10-19 18:15:54 -0400

    display username

--- express_server.js
+++ express_server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const cookieParser = require('cookie-parser')
 const app = express();
+app.use(cookieParser())
 const PORT = 8080; // default port 8080
 
 const urlDatabase = {
@@ -24,16 +26,26 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    username: req.cookies["username"],
+    urls: urlDatabase,
+  };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"],
+  }
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { 
+    id: req.params.id, 
+    longURL: urlDatabase[req.params.id],
+    username: req.cookies["username"],
+  };
   res.render("urls_show", templateVars);
 });
 
@@ -51,7 +63,7 @@ app.post("/urls", (req, res) => {
   const id = generateRandomString()
   urlDatabase[id] = {
     longURL,
-  }
+  };
   res.redirect(`/urls/${id}`);
 });
 
