c015af27504b9d2e82f7c9b9ec9597273520b713
Patrick Aucoin <aucoinp640@gmail.com>
2023-10-19 13:41:44 -0400

    complete URL shortening Part 1

--- express_server.js
+++ express_server.js
@@ -2,12 +2,22 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.set("view engine", "ejs");
-
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  const alphanumeric = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let result = '';
+  for (let i = 0; i < 6; i++) {
+    result += alphanumeric.charAt(Math.floor(Math.random() * alphanumeric.length));
+  }
+};
+
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
@@ -16,6 +26,10 @@ app.get("/urls.json", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
@@ -27,3 +41,7 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
